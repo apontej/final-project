2a2151df11f3ec850edfed7cad9e2d1f7c0c4a72
apontej <aponte@fas.harvard.edu>
2014-04-18 10:23:17 -0400

    further edits to teamcolors, poll_graph, new-master-id

--- scripts/teamcolors.js
+++ scripts/teamcolors.js
@@ -7,7 +7,7 @@ d3.text("data/teamcolors.csv", function(error, data) {
 function searchColor(school) {
 	for (var i=0;i<teamcolors.length;i++) {
 		if (teamcolors[i][0] === school) {
-			if ((teamcolors[i][1] === "#FFFFFF") || (teamcolors[i][1] === "#ffffff") || (teamcolors[i][1] === "#000000")) {
+			if ((teamcolors[i][1] === "#FFFFFF")/* || (teamcolors[i][1] === "#000000")*/) {
 				return teamcolors[i][2];
 			} else if (teamcolors[i][1] === "") {
 				return "#000000";
